8b8c9e1960edee99525facfa6cef3bf7e9e3e1a4
OS <91291661+TA2021A@users.noreply.github.com>
2021-10-08 20:43:28 -0400

    hw3+Behave features and steps

---
+++ hw3.py
@@ -0,0 +1,23 @@
+
+"""Amazon logo"""
+driver.find_element(By.CSS_SELECTOR, "i.a-icon.a-icon-logo")
+
+"""Create account"""
+driver.find_element(By.CSS_SELECTOR, "h1.a-spacing-small")
+
+"""Your name"""
+driver.find_element(By.CSS_SELECTOR, "input#ap_customer_name")
+
+"""EMAIL"""
+driver.find_element(By.CSS_SELECTOR, "input#ap_email")
+
+"""Password"""
+driver.find_element(By.CSS_SELECTOR, "input#ap_password")
+
+"""Re-enter password"""
+driver.find_element(By.CSS_SELECTOR, "input#ap_password_check")
+
+"""Create your Amazon account"""
+driver.find_element(By.CSS_SELECTOR, "input#continue")
+
+
